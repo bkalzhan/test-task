b2547c91e35a97206923ce1c22fe81b1e0cc56b5
bkalzhan <47266416+bkalzhan@users.noreply.github.com>
2022-09-14 11:58:09 +0600

    additional commit

--- emloyee/models.py
+++ emloyee/models.py
@@ -11,7 +11,7 @@ position_type = (
     ("INTERN", "Intern")
 )
 
-
+# models
 class Employee(models.Model):
     first_name = models.CharField("First name", null=True, blank=True, max_length=20)
     last_name = models.CharField("Last name", null=True, blank=True, max_length=20)
